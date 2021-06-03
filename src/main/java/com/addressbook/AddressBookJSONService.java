a985fc1b25ffad5d689df5b744104182da47b273
Babakailash <nathkailash2020@gmail.com>
2021-06-03 15:56:29 +0530

    Added:Multiple Details in AddressBook

--- src/main/java/com/addressbook/AddressBookJSONService.java
+++ src/main/java/com/addressbook/AddressBookJSONService.java
@@ -3,15 +3,24 @@ package com.addressbook;
 import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class AddressBookJSONService {
 
     public AddressBookData[] readAddressBookData() {
         RestAssured.baseURI = "http://localhost:3000";
-
         Response response = RestAssured.get("/addressbook");
         System.out.println("Contacts in JSON are : " + response.asString());
-        AddressBookData[] addressBookData = new Gson().fromJson(response.asString(),AddressBookData[].class);
+        AddressBookData[] addressBookData = new Gson().fromJson(response.asString(), AddressBookData[].class);
         return addressBookData;
+    }
+
+    public Response addContactstoJSONServer(AddressBookData addressBookData){
+        RestAssured.baseURI = "http://localhost:3000";
+        String contact = new Gson().toJson(addressBookData);
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.header("Content-Type", "application/json");
+        requestSpecification.body(contact);
+        return requestSpecification.post("/addressbook");
     }
 }
--- src/test/java/com/addressbook/AddressBookDataJSONTest.java
+++ src/test/java/com/addressbook/AddressBookDataJSONTest.java
@@ -1,5 +1,6 @@
 package com.addressbook;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,21 @@ public class AddressBookDataJSONTest {
         AddressBookJSONService addressBookJSONService = new AddressBookJSONService();
         AddressBookData[] addressBookData = addressBookJSONService.readAddressBookData();
         System.out.println(addressBookData);
-        Assertions.assertEquals(3, addressBookData.length);
-        }
+        Assertions.assertEquals(5, addressBookData.length);
+    }
+
+    @Test
+    public void whenNewContactisInsertedShouldReturnResponseCode201() {
+        AddressBookJSONService addressBookJSONService = new AddressBookJSONService();
+        AddressBookData store = new AddressBookData(6, "Arvind", "Kejriwal", "Moti-Road", "Kaushambi",
+                                            "New Delhi", "110096", "444444444", "arvind@kejri.com", "2014-06-25");
+        Response response = addressBookJSONService.addContactstoJSONServer(store);
+        int statusCode = response.statusCode();
+        System.out.println(statusCode);
+        AddressBookData[] addressBookData = addressBookJSONService.readAddressBookData();
+        System.out.println(addressBookData);
+        Assertions.assertEquals(201, statusCode);
+    }
 }
 
 
